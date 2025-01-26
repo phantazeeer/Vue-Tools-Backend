053969b75977f93b82d16faa8d603b1b694f0e40
phantazeeer <shturmovik729@gmail.com>
2025-01-26 18:59:08 +0400

    routes

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
+from app.api.schemas import UserCreate, UserResponse
 
-router = APIRouter(tags=["work with users"])
+router = APIRouter(tags=["work with users"], prefix="/api")
+
+
+@router.post("/register")
+async def register(user: UserCreate) -> UserResponse:
+    """Регистрация пользователя"""
+    pass
+
 
 @router.get("/users")
-async def get_users():
-    """Возвращает данные пользователя"""
-    return None
+async def get_users() -> UserResponse:
+    pass
